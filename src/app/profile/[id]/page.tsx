6394cf8f6ee3a3ff11978a4241d9f56e0b9054d2
Ajinkya-kingre <145644231+Ajinkya-kingre@users.noreply.github.com>
2024-02-08 16:56:51 +0530

    add to github

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,12 @@
+export default function userProfile({ params }: any) {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p className="text-4xl">
+        profile page
+        <span className="p-2 text-black rounded-md bg-orange-500 ml-2" >{params.id}</span>
+      </p>
+    </div>
+  );
+}
